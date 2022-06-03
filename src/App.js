920c86b46ba3e82f521e961882c12dfe33a5ceb4
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-06-03 17:59:05 -0400

    Make scrollable div more componentised

--- src/App.js
+++ src/App.js
@@ -7,8 +7,26 @@ function App() {
   return (
 	<ThemeProvider>
 		<div className="App">
-		  <TopBar />
-		  <ScrollableDiv />
+		    <TopBar />
+		    <ScrollableDiv textData = {[
+			    <div className = "Slide">
+					Lorem ipsum dolor sit amet <br />
+					asdasd
+				</div>,
+				<div className = "Slide">
+					Lorem2 ipsum dolor sit amet <br />
+					asdasd2
+				</div>
+		    ]} imageData = {[
+				<div className = "Slide">
+					Lorem isdasdpsum dolor sit amet <br />
+					asdasdasd
+				</div>,
+				<div className = "Slide">
+					Lorem2 ipsum dolor sit amet <br />
+					asdasd2as
+				</div>
+			]}/>
 		</div>
 	</ThemeProvider>
   );
